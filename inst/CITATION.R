d3b006042bc7869d13fdc3965129550ef3000957
phytomosaic <smithr2@oregonstate.edu>
2019-06-06 07:44:08 -0700

    Add CITATION file

---
+++ inst/CITATION.R
@@ -0,0 +1,17 @@
+citHeader("To cite in publications, please use:")
+
+citEntry(entry        = "manual",
+         title        = "{grlyr}: Package for Ground Layer Estimation",
+         author       = personList(as.person("Robert J. Smith")),
+         organization = "Oregon State University",
+         address      = "Corvallis, Oregon, USA",
+         note         = "version 0.0.0-2019-06-05",
+         year         = "2019",
+         url          = "https://github.com/phytomosaic/grlyr",
+         textVersion  = paste("Smith, R.J. 2019.",
+                              "grlyr: Package for Ground Layer Estimation.",
+                              "version 0.0.0-2019-06-05.",
+                              "Oregon State University,",
+                              "Corvallis, Oregon, USA.",
+                              "https://github.com/phytomosaic/grlyr")
+)
