d5addd6e417f73bd97f393aab535ee6cf5b85f37
phytomosaic <smithr2@oregonstate.edu>
2019-06-07 11:12:31 -0700

    Clean documentation for summary_plot

--- R/summary_plot.R
+++ R/summary_plot.R
@@ -1,15 +1,9 @@
-#' @title Summary by functional group
+#' @title Summary by plots
 #'
 #' @description
-#' Summary of ground layer observations, disaggregated by functional
-#'      groups.
+#' Summary of ground layer observations at the plot-level.
 #'
 #' @param x \code{"grlyr"} object from \code{calc_biomass()}.
-#'
-#' @param eachplot default \code{FALSE} returns values as the mean
-#'      across all plots in \code{x}; \code{TRUE} returns values as
-#'      the mean of each functional group within each plot (and may
-#'      exclude functional groups unobserved on a given plot).
 #'
 #' @param ... further arguments (currently ignored).
 #'
@@ -26,20 +20,10 @@
 #' @examples
 #' data(est)
 #' x <- calc_biomass(est)
-#' summary_fg(x)
-#' summary_fg(x, eachplot=TRUE)
+#' s <- summary_plot(x)
 #'
+#' @export
 #' @rdname summary_plot
-# ### unexported SE, returns '0' instead of NA for zero-length vectors
-#`se` <- function(a, na.rm=TRUE) {
-#        if (length(a) < 2) {
-#                return(0L)
-#        } else {
-#                sd(a, na.rm=na.rm) / sqrt(length(na.omit(a))-1)
-#        }
-#}
-##' @export
-##' @rdname summary_plot
 ### summarize for PLOTS:
 `summary_plot` <- function(x, ...){
      if (!inherits(x, "grlyr")) {
