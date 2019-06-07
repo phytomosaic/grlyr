608efe2873f86528d90ec3860af3f72f20c159b1
phytomosaic <smithr2@oregonstate.edu>
2019-06-07 10:56:09 -0700

    Add summary_plot function

--- R/summary_plot.R
+++ R/summary_plot.R
@@ -40,5 +40,55 @@
 #}
 ##' @export
 ##' @rdname summary_plot
-#`summary_plot` <- function(...) { NULL }
-NULL
+### summarize for PLOTS:
+`summary_plot` <- function(x, ...){
+     if (!inherits(x, "grlyr")) {
+          stop('must inherit from `grlyr`')
+     }
+     cat('step 1 of 3\n')
+     # first, sum per mq for ea 'type', then both moss/lich summed
+     y <- plyr::ddply(x, plyr::.(mpid, type), plyr::summarize,
+                      mq_mass = sum(mass, na.rm=TRUE),
+                      .progress='time')
+     y <- reshape2::dcast(
+          y, mpid ~ type, fill=0, value.var=c('mq_mass'))
+     y$total <- y$lich + y$moss
+     y <- plyr::join(x, y, by=c('mpid'), type='full', match='all')
+     y <- plyr::rename(y, c('moss'='mq_moss', 'lich'='mq_lich',
+                            'total'='mq_total'))
+     cat('step 2 of 3\n')
+     # sum *within* microquads
+     y <- plyr::ddply(y, plyr::.(mpid), plyr::mutate,
+                      mq_c      = sum(predC, na.rm=TRUE), # g/mq
+                      mq_n      = sum(predN, na.rm=TRUE), # g/mq
+                      mq_vol    = sum(volume,na.rm=TRUE), # cm3/mq
+                      mq_cover  = sum(cover, na.rm=TRUE), # %/mq
+                      .progress='time')
+     cat('step 3 of 3\n')
+     # aggregate to plot-level
+     d <- plyr::ddply(y, plyr::.(plot), plyr::summarize,
+                      total_mn = mean(mq_total*100,na.rm=T),# kg/ha
+                      total_sd =   sd(mq_total*100,na.rm=T),# kg/ha
+                      lich_mn  = mean(mq_lich*100,na.rm=T), # kg/ha
+                      lich_sd  =   sd(mq_lich*100,na.rm=T), # kg/ha
+                      moss_mn  = mean(mq_moss*100,na.rm=T), # kg/ha
+                      moss_sd  =   sd(mq_moss*100,na.rm=T), # kg/ha
+                      c_mn     = mean(mq_c*100,na.rm=T),    # kg/ha
+                      c_sd     =   sd(mq_c*100,na.rm=T),    # kg/ha
+                      n_mn     = mean(mq_n*100,na.rm=T),    # kg/ha
+                      n_sd     =   sd(mq_n*100,na.rm=T),    # kg/ha
+                      vol_mn   = mean(mq_vol*0.1,na.rm=T),  # m3/ha
+                      vol_sd   =   sd(mq_vol*0.1,na.rm=T),  # m3/ha
+                      cover_mn = mean(mq_cover,na.rm=T),    # %
+                      cover_sd =   sd(mq_cover,na.rm=T),    # %
+                      depth_mn = mean(depth, na.rm=T),      # cm
+                      depth_sd =   sd(depth, na.rm=T),      # cm
+                      .progress='time')
+     # functional group richness per plot
+     d$fgr <- plyr::ddply(y[y$covercm != '0',,drop = T],
+                          plyr::.(plot), plyr::summarize,
+                          fgr=length(unique(fg)))$fgr
+     # order by plot
+     d <- plyr::arrange(d, plot)
+     d
+}
