a6e74e6619c79f1bfb90e4bcde5e5536ce66f1ac
phytomosaic <smithr2@oregonstate.edu>
2019-06-09 12:44:32 -0700

    Add help info to the app

--- inst/shiny_examples/grlyr_web/app.R
+++ inst/shiny_examples/grlyr_web/app.R
@@ -1,141 +1,178 @@
-# Shiny web application for grlyr: Ground Layer Estimation
+####################################################################
+###
+###   Shiny web application for grlyr: Ground Layer Estimation
+###
+####################################################################
 
 require(shiny)
 require(grlyr)
 
-`ui` <- fluidPage(
-     fluidPage(
+u1 <- paste0(
+        'https://www.fs.fed.us/pnw/rma/fia-topics/',
+        'documentation/field-manuals/documents/Annual/',
+        '2018_FIA_Interior_Alaska_Supplement.pdf')
+u2 <- 'https://github.com/phytomosaic/grlyr'
+acc <- c('text/csv', 'text/comma-separated-values,text/plain', '.csv')
 
-          ### title and headers
-          titlePanel(
-               div(HTML("<em>grlyr</em> : Ground Layer Estimation"))
-          ),
-          br(),
-          h4("Estimates biomass, carbon and nitrogen of moss and
-        lichen ground layers as part of the US Forest Service's Forest
-        Inventory and Analysis program."),
-          br(),
+`ui` <- fluidPage(fluidPage(
 
-          ### main page has sidebar
-          sidebarLayout(
+        ### title and headers
+        titlePanel(
+                div(HTML('<em>grlyr</em> : Ground Layer Estimation'))
+        ),
+        br(),
+        h4('Estimates biomass, carbon and nitrogen of moss and
+        lichen ground layers, as part of the US Forest Service\'s
+        Forest Inventory and Analysis program.'),
+        br(),
 
-               ### sidebar (left)
-               sidebarPanel(
-                    h3("Upload"),
-                    actionButton("show", "File requirements"),
-                    br(),br(),
-                    fileInput(
-                         'file1', 'Choose CSV file',
-                         accept=c(
-                              'text/csv',
-                              'text/comma-separated-values,text/plain',
-                              '.csv')),
-                    tags$hr(),
-                    h3("Download"),
-                    'Summaries are automatic, download them here:',
-                    br(),br(),
-                    downloadButton('downloadData', 'Download'),
-                    width = 4),
+        ### setup sidebar layout
+        sidebarLayout(
 
-               ### main panel (right, upper)
-               mainPanel(
+                ### sidebar (left)
+                sidebarPanel(h3('Upload'),
+                             actionButton('show','File requirements'),
+                             br(),br(),
+                             fileInput('file1', NULL, accept=acc,
+                                       buttonLabel=list(
+                                               icon('upload'),
+                                               'Upload CSV')
+                             ),
+                             tags$hr(),
+                             h3('Download'),
+                             'Summaries are automatic,
+                             download them here:',
+                             br(),br(),
+                             downloadButton('downloadData','Download'),
+                             tags$hr(),
+                             h3('Further info'),
+                             p('Source code: '),
+                             p(a(u2, href = u2)),
+                             p('Contact maintainer: '),
+                             p(a('smithr2@oregonstate.edu',
+                                 href = 'smithr2@oregonstate.edu')),
+                             p('Sampling protocol: '),
+                             p(a('Download PDF',
+                                 href = u1)),
+                             br(),
+                             width = 4),
 
-                    ### upper
-                    tableOutput('plot_summaries_contents')
-
-                    # ### lower
-                    # tags$hr(),
-                    # tableOutput('fg_summaries_contents ')
-
-               )
-
-          ),
-
-          ### footer
-          p('Further information at: ',
-            a("https://github.com/phytomosaic/grlyr",
-              href = "https://github.com/phytomosaic/grlyr")),
-          p('Contact maintainer at: ',
-            a("smithr2@oregonstate.edu",
-              href = "smithr2@oregonstate.edu")),
-          br(),br(),br()
-     )
+                ### main panel (right, upper)
+                mainPanel(h3(textOutput('hdr1')),
+                          tableOutput('plot_summaries_contents'),
+                          tags$hr(),
+                          h3(textOutput('hdr2')),
+                          tableOutput('fg_summaries_contents')
+                )
+        )
+)
 )
 
 `server` <- function(input, output) {
 
-     ### observe button click to show dialog box
-     observeEvent(input$show, {
-          showModal(modalDialog(
-               title = "File requirements",
-               p("Your uploaded file must be:"),
-               p("a CSV file in \"long\" format (each row is one
-            observation of a functional group in one microquad)."),
-               p("Column names, and their values, must be:"),
-               tags$ol(
-                    tags$li("`plot` = anything"),
-                    tags$li("`microquad` = integers 1 thru 32"),
-                    tags$li("`fg` = CC, CO, LF, LLFOL, LLFRU, LNFOL,
-                LNFRU, MF, MN, MS, MT, VF, VS"),
-                    tags$li("`cover` = 0, 0.1, 1, 2, 5, 10, 25,
-                    50, 75, 95, 99"),
-                    tags$li("`depth` = 0, 0.125, 0.25, 0.5, 1, 2,
-                            4, 8, 16")
-               ),
-               p("Other values may \"work\" but give nonsense. Missing
-              values not permitted in first 5 columns."),
-               easyClose = TRUE
-          ))
-     })
+        ### observe button click to show dialog box
+        observeEvent(input$show, {showModal(
+                modalDialog(
+                        title = 'File requirements',
+                        p('Your uploaded file must be a CSV file in
+                        \'long\' format (each row is one observation
+                        of a functional group in one microquad).'),
+                        p('Column names, and their values, must be:'),
+                        tags$pre('`plot`\t\t= any values
+`microquad`\t= integers\t= 1 thru 32
+`fg`\t\t= categories\t= CC, CO, LF, LLFOL, LLFRU, LNFOL, LNFRU,
+\t\t\t\t\t MF, MN, MS, MT, VF, VS
+`cover`\t\t= percentages\t= 0, 0.1, 1, 2, 5, 10, 25, 50, 75, 95, 99
+`depth`\t\t= inches\t= 0, 0.125, 0.25, 0.5, 1, 2, 4, 8, 16'),
+                        p('Other values may \'work\' but give
+                        nonsense. Missing values not permitted in
+                          first 5 columns.'),
+                        easyClose = TRUE
+                ))
+        })
 
+        ### reactive expression for user-selected CSV file input
+        CALCS <- reactive({
+                inFile <- input$file1
+                if (is.null(input$file1)) {
+                        return(NULL)
+                } else {
+                        est <- read.csv(inFile$datapath, header = T)
+                        x   <- grlyr::calc_biomass( est )
+                        o1  <- grlyr::summary_plot(x)
+                        o2  <- grlyr::summary_fg(x)
+                        list(o1 = o1, o2 = o2)
+                }
+        })
 
-     ### observe file input, to calculate plot summaries
-     get_plot_summaries <- reactive({
-          inFile <- input$file1
-          if (is.null(input$file1)) return(NULL)
-          # read user-selected CSV
-          est <- read.csv(inFile$datapath, header = TRUE)
-          # calculate biomass using the calc_biomass function
-          x <- grlyr::calc_biomass(est)
-          # summary by plots
-          s <- grlyr::summary_plot(x)
-          return(s)
-     })
-     ### render the plot summaries
-     output$plot_summaries_contents <- renderTable(
-          get_plot_summaries()
-     )
+        ### render PLOT summaries
+        output$plot_summaries_contents <- renderTable(
+                if (is.null( CALCS() )) {
+                        return(NULL)
+                } else {
+                        # head(CALCS()$o1)
+                        from <- c('plot',
+                                  'total_mn', 'total_sd',
+                                  'lich_mn', 'lich_sd',
+                                  'moss_mn', 'moss_sd',
+                                  'c_mn', 'c_sd',
+                                  'n_mn', 'n_sd',
+                                  'vol_mn', 'vol_sd',
+                                  'cover_mn', 'cover_sd',
+                                  'depth_mn', 'depth_sd',
+                                  'fgr')
+                        to <- c('Plot_id',
+                                'Mean_biomass', 'SD_biomass',
+                                'Mean_biomass_lichen',
+                                'SD_biomass_lichen',
+                                'Mean_biomass_mosses',
+                                'SD_biomass_mosses',
+                                'Mean_C', 'SD_C',
+                                'Mean_N', 'SD_N',
+                                'Mean_volume', 'SD_volume',
+                                'Mean_cover', 'SD_cover',
+                                'Mean_depth', 'SD_depth',
+                                'Functional_group_richness')
+                        xx <- CALCS()$o1
+                        colnames(xx)[colnames(xx) == from] <- to
+                        head( xx )
+                }
+        )
 
+        ### render FUNCTIONAL GROUP summaries
+        output$fg_summaries_contents <- renderTable(
+                if (is.null( CALCS() ))
+                        return(NULL)
+                else
+                        CALCS()$o2
+        )
 
-     # ### observe file input, to calculate functional grp summaries
-     # get_fg_summaries <- reactive({
-     #      inFile <- input$file1
-     #      if (is.null(input$file1)) return(NULL)
-     #      # read user-selected CSV
-     #      est <- read.csv(inFile$datapath, header = TRUE)
-     #      # calculate biomass using the calc_biomass function
-     #      x <- grlyr::calc_biomass(est)
-     #      # summary by plots
-     #      s <- grlyr::summary_fg(x)
-     #      s
-     # })
-     # ### render the functional grp  summaries
-     # output$fg_summaries_contents <- renderTable(
-     #      get_fg_summaries()
-     # )
+        ### dynamic headers only appear after summaries done
+        output$hdr1 <- renderText(
+                if (is.null( CALCS() ))
+                        return(NULL)
+                else
+                        paste0('Preview plot summaries')
 
-     ### download the plot summaries
-     output$downloadData <- downloadHandler(
-          filename = function() {
-               paste("grlyr_summary_",
-                     # Sys.Date(),
-                     format(Sys.time(), "%Y-%m-%d_%H-%M"),
-                     ".csv", sep='')
-          },
-          content = function(file) {
-               write.csv(get_plot_summaries(), file, row.names = FALSE)
-          })
+        )
+        output$hdr2 <- renderText(
+                if (is.null( CALCS() ))
+                        return(NULL)
+                else
+                        paste0('Functional group summaries')
 
+        )
+
+        ### download the plot summaries
+        output$downloadData <- downloadHandler(
+                filename = function() {
+                        paste('grlyr_summary_',
+                              format(Sys.time(), '%Y-%m-%d_%H-%M'),
+                              '.csv', sep='')
+                },
+                content = function(file) {
+                        write.csv(CALCS()$o1, file, row.names = FALSE)
+                })
 }
 
 # ### run the shiny app
